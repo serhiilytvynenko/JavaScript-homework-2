930ea592d21c713e6eeb0f83d7694693b0bc1b62
serhii <serezhalytvynenko@gmail.com>
2023-05-16 19:17:07 +0200

    rate correct code

--- src/rate.js
+++ src/rate.js
@@ -1,24 +1,27 @@
 "use strict";
 function rate(winRate, loseRate) {
+  let diffRate = winRate - loseRate;
   if (
+    Number.isNaN(diffRate) ||
     typeof winRate !== "number" ||
     typeof loseRate !== "number" ||
     winRate < 0 ||
     loseRate < 0
   ) {
-    console.log("Enter wrong information");
-  } else if (winRate >= loseRate) {
-    if (winRate - loseRate >= 0 && winRate - loseRate <= 2) {
-      return winRate + 2;
-    } else if (winRate - loseRate > 2 && winRate - loseRate < 20) {
-      return winRate + 1;
-    } else if (winRate - loseRate >= 20) {
-      return winRate;
-    }
-  } else if (winRate < loseRate && winRate !== 0) {
-    return (loseRate - winRate + 5) / 3;
-  } else {
-    return loseRate;
+    return "Entered wrong information";
   }
+  if (diffRate >= 0 && diffRate <= 2) {
+    return (winRate += 2);
+  }
+  if (diffRate > 2 && diffRate < 20) {
+    return (winRate += 1);
+  }
+  if (diffRate >= 20) {
+    return winRate;
+  }
+  if (diffRate < 0 && winRate !== 0) {
+    return winRate + Math.round((Math.abs(diffRate) + 5) / 3);
+  }
+  loseRate;
 }
-console.log(rate(30, 50));
+console.log(rate(-100, 35));
