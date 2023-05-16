fe2529298edeaac0de89b94c29f6874020f3f8e6
serhii <serezhalytvynenko@gmail.com>
2023-05-16 20:24:03 +0200

    twofold older correct code

--- src/father-twofold-older.js
+++ src/father-twofold-older.js
@@ -6,12 +6,20 @@
  * @returns {number} year when father was/will be twofold older than son
  */
 
-const ageDifference = () => {
+const ageDifference = (fatherAge, sonAge) => {
   let years = 0;
-  let fatherAge = Number(prompt("Enter father age"));
-  let sonAge = Number(prompt("Enter son age"));
-  if (fatherAge - sonAge < 15 || sonAge <= 0) {
+  if (
+    fatherAge - sonAge < 15 ||
+    sonAge < 0 ||
+    typeof fatherAge !== "number" ||
+    typeof sonAge !== "number" ||
+    Number.isNaN(fatherAge) ||
+    Number.isNaN(sonAge)
+  ) {
     return "You entered wrong information";
+  }
+  if(fatherAge / sonAge === 2){
+    return years;
   }
   if (fatherAge / sonAge > 2) {
     while (fatherAge / sonAge !== 2) {
@@ -29,4 +37,39 @@ const ageDifference = () => {
   return `${years} years ago father was twofold older then son`;
 };
 
-console.log(ageDifference());
+console.log(ageDifference(20, 0));
+
+
+
+
+//============================================================================
+
+// const ageDifference = (fatherAge, sonAge, years = 0) => {
+//   if (
+//     fatherAge - sonAge < 15 ||
+//     sonAge < 0 ||
+//     typeof fatherAge !== "number" ||
+//     typeof sonAge !== "number" ||
+//     Number.isNaN(fatherAge) ||
+//     Number.isNaN(sonAge)
+//   ) {
+//     return "You entered wrong information";
+//   }
+  
+//   if (fatherAge / sonAge === 2) {
+//     return years;
+//   }
+  
+//   if (fatherAge / sonAge > 2) {
+//     return ageDifference(fatherAge + 1, sonAge + 1, years + 1);
+//   }
+  
+//   return ageDifference(fatherAge - 1, sonAge - 1, years + 1);
+// };
+
+// console.log(ageDifference(26, 10));
+
+// Нашел только такой вариант сделать код короче, но рекурсию еще не учили вроде
+
+
+
